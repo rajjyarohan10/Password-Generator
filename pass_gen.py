4a517cfd43b67280c13320ed3581c9e92ccea3de
Rajjya Rohan Paudyal <84804921+rajjyarohan10@users.noreply.github.com>
2022-11-11 11:50:09 -0500

    First commit

---
+++ pass_gen.py
@@ -0,0 +1,14 @@
+"""
+    Date: 11/11/2022
+    File: pass_gen.py
+    Description: Testing out a new idea of creating my own password generator
+"""
+import random
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+symbols = "!@#$%^&*()_+-{}[]|\/?><"
+all_mix = uppercase_letters + lowercase_letters + symbols
+pw_length = 10
+
+password = "".join(random.sample(all_mix, pw_length))
+print("Generated Password of length",  pw_length, "=", password)
